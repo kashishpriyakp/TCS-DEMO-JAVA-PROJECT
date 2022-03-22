ee562a5544979c6fa3005a34ce2830d2be798daa
kashishpriyakp <kashishpriya22@gmail.com>
2022-03-21 22:31:15 -0700

    First Commit for demotcs-java-project

---
+++ DemoTCS-JAVA-Project/src/com/tcs/demo/DemoTtest.java
@@ -0,0 +1,11 @@
+package com.tcs.demo;
+
+public class DemoTtest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+
+	}
+
+}
---
+++ DemoTCS-JAVA-Project/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author springuser07
+ *
+ */
+module demoTCS {
+}
